ab57f8ab6e4a7fbb447e76463525ae30151f3858
RoboMagus <68224306+RoboMagus@users.noreply.github.com>
2022-05-04 21:16:50 +0200

    tiny innitial boilerplate update

--- octoprint_preheathelper/__init__.py
+++ octoprint_preheathelper/__init__.py
@@ -1,20 +1,31 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
+import copy
+import sys, traceback
+import time
 import octoprint.plugin
 
-class PreheathelperPlugin(octoprint.plugin.SettingsPlugin,
-    octoprint.plugin.AssetPlugin,
-    octoprint.plugin.TemplatePlugin
-):
+from octoprint.events import eventManager, Events
+
+class PreheathelperPlugin(  octoprint.plugin.SettingsPlugin,
+                            octoprint.plugin.AssetPlugin,
+                            octoprint.plugin.TemplatePlugin,
+                            octoprint.plugin.EventHandlerPlugin ):
+
+    def preprocess_loaded_file(self, full_filename):
+        self._logger.info(f"Preprocessing file: {full_filename}")
+
+    ##~~ EventHandlerPlugin mixin
+
+    def on_event(self, event, payload):
+		if event == Events.FILE_SELECTED:
+            if self._settings.get(["preheat_on_file_load"]) and payload["origin"] == "local":
+                #   Payload:
+                #       name: the file’s name
+                #       path: the file’s path within its storage location
+                #       origin: the origin storage location of the file, either local or sdcard
+                self.preprocess_loaded_file(payload["path"])
 
     ##~~ SettingsPlugin mixin
 
@@ -23,15 +34,20 @@ class PreheathelperPlugin(octoprint.plugin.SettingsPlugin,
             # put your plugin's default settings here
         }
 
+    def on_settings_initialized(self):
+        self._logger.debug("on_settings_initialized()")
+
+        
+    def on_settings_save(self, data):
+        self._logger.debug(f"PreHeatHelper settings saved: {data}")
+
     ##~~ AssetPlugin mixin
 
     def get_assets(self):
         # Define your plugin's asset files to automatically include in the
         # core UI here.
         return {
-            "js": ["js/preheathelper.js"],
-            "css": ["css/preheathelper.css"],
-            "less": ["less/preheathelper.less"]
+            "js": ["js/preheathelper.js"]
         }
 
     ##~~ Softwareupdate hook
@@ -56,21 +72,26 @@ class PreheathelperPlugin(octoprint.plugin.SettingsPlugin,
             }
         }
 
+    ##~~ Plugin Helper functions
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Preheathelper Plugin"
+    def dummy_func(self):
+        return None
 
 
-# Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
-# OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
-# OctoPrint 1.8.0 onwards only supports Python 3.
+
+__plugin_name__ = "PreHeat Helper"
 __plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
 
 def __plugin_load__():
+    plugin = PreheathelperPlugin()
+
+    global __plugin_helpers__
+    __plugin_helpers__ = dict(
+        dummy_func = plugin.dummy_func
+    )
+
     global __plugin_implementation__
-    __plugin_implementation__ = PreheathelperPlugin()
+    __plugin_implementation__ = plugin
 
     global __plugin_hooks__
     __plugin_hooks__ = {
